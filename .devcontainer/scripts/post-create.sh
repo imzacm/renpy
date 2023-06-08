2ffcc45bc5eac681f41f2a60185a9591c155ccde
Zac McChesney <imzacm@gmail.com>
2023-06-08 12:28:40 +0000

    Created a devcontainer image which might work

---
+++ .devcontainer/scripts/post-create.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [ ! -d pygame_sdl2/.git ]; then
+    [ -d pygame_sdl2 ] && rm -r pygame_sdl2
+    git clone --depth 1 https://www.github.com/renpy/pygame_sdl2
+else
+    pushd pygame_sdl2
+    git pull
+    popd
+fi
+
+pushd pygame_sdl2
+python setup.py install
+python setup.py install_headers
+popd
+
+export RENPY_DEPS_INSTALL="/usr:/usr/lib/$(gcc -dumpmachine)/"
+export RENPY_CYTHON=cython
+
+printf 'export RENPY_DEPS_INSTALL=%s\n' "'${RENPY_DEPS_INSTALL}'" >> /etc/bash.bashrc
+printf 'export RENPY_CYTHON=%s\n' "'${RENPY_CYTHON}'" >> /etc/bash.bashrc
+
+# pushd module
+# python setup.py install
+# popd
